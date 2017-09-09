cf51413c4609e04eeab81b58dd58451ce772c119
Patrick Pacher <p.pacher@cybertrap.com>
2017-09-09 13:15:50 +0200

    Added Authenticator interface

--- provider/provider.go
+++ provider/provider.go
@@ -17,6 +17,15 @@ var (
 	ErrDuplicateIdentity = errors.New("duplicated itentity")
 )
 
+// Authenticator authenticates a client using a password
+type Authenticator interface {
+	// Has returns true if the authenticator has a secret for `client`
+	Has(client string) bool
+
+	// Verify verifies if `password` is valid for `client`
+	Verify(client string, password string) (bool, error)
+}
+
 // Provider for identities
 type Provider interface {
 	// Identities returns all identities the provider knows about
